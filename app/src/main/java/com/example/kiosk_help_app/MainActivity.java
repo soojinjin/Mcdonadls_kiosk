2a99bc46d33ba580c9af17e7b0f282e4dd7573e0
soojinjin <imsoojin321@naver.com>
2023-05-24 00:56:49 +0900

    챗봇 연결 필요

--- app/src/main/java/com/example/kiosk_help_app/MainActivity.java
+++ app/src/main/java/com/example/kiosk_help_app/MainActivity.java
@@ -2,25 +2,72 @@ package com.example.kiosk_help_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bae.dialogflowbot.adapters.ChatAdapter;
+import com.example.kiosk_help_app.helpers.SendMessageInBg;
+import com.example.kiosk_help_app.interfaces.BotReply;
+import com.example.kiosk_help_app.models.Message;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+import com.google.cloud.dialogflow.v2.QueryInput;
+import com.google.cloud.dialogflow.v2.SessionName;
+import com.google.cloud.dialogflow.v2.SessionsClient;
+import com.google.cloud.dialogflow.v2.SessionsSettings;
+import com.google.cloud.dialogflow.v2.TextInput;
+import com.google.common.collect.Lists;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 import com.example.kiosk_help_app.alone.fastfood.AloneFastfoodSelectStorePackageActivity;
-import com.example.kiosk_help_app.alone.fastfood.AloneFastfoodStoreActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BotReply {
+    TextToSpeech _tts;
+    RecyclerView chatView; //챗봇 화면
+    ChatAdapter chatAdapter;
+    List<Message> messageList = new ArrayList<>();
+    EditText editMessage;
+    ConstraintLayout constraintLayout;
+    ConstraintLayout constraintLayout2;
+    ImageView imageView;
 
+    //dialogFlow
+    private SessionsClient sessionsClient;
+    private SessionName sessionName; // session name은 uuid와 project id를 사용
+    private String uuid = UUID.randomUUID().toString(); //유일한 식별자 생성
+    private String TAG = "mainactivity"; //현재 사용하는 클래스에 tag상수 선언 규칙칙
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chatView = findViewById(R.id.chatView);
+        editMessage = findViewById(R.id.editMessage);
+        chatAdapter = new ChatAdapter(messageList, this);
+        chatView.setAdapter(chatAdapter);
+        Button alone_btn = findViewById(R.id.btn_start);
+        constraintLayout = findViewById(R.id.constraintLayout);
 
-        //혼자하기 버튼을 클릭했을때
-        Button alone_btn =  findViewById(R.id.btn_start);
+
+
+        constraintLayout.setVisibility(View.VISIBLE);
+        editMessage.setVisibility(View.GONE);
+
+
         alone_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,9 +75,53 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        setUpBot();
     }
 
 
 
+            private void setUpBot() {
+        try {
+            InputStream stream = this.getResources().openRawResource(R.raw.credential);
+            GoogleCredentials credentials = GoogleCredentials.fromStream(stream)
+                    .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+            String projectId = ((ServiceAccountCredentials) credentials).getProjectId();
+
+            SessionsSettings.Builder settingsBuilder = SessionsSettings.newBuilder();
+            SessionsSettings sessionsSettings = settingsBuilder.setCredentialsProvider(
+                    FixedCredentialsProvider.create(credentials)).build();
+            sessionsClient = SessionsClient.create(sessionsSettings);
+            sessionName = SessionName.of(projectId, uuid);
+
+            Log.d(TAG, "projectId : " + projectId);
+        } catch (Exception e) {
+            Log.d(TAG, "setUpBot: " + e.getMessage());
+        }
+    }
+
+    // message 를 봇에게 보내는 역할을 한다
+    private void sendMessageToBot(String message) {
+        QueryInput input = QueryInput.newBuilder()
+                .setText(TextInput.newBuilder().setText(message).setLanguageCode("ko-KR")).build();
+        new SendMessageInBg(this, sessionName, sessionsClient, input).execute();
+    }
+
+
+
+//답변을 가져오는 부분
+            @Override
+    public void callback(DetectIntentResponse returnResponse) {
+        if(returnResponse!=null) {
+            String botReply = returnResponse.getQueryResult().getFulfillmentText();
+            if(!botReply.isEmpty()){
+                messageList.add(new Message(botReply, true));
+                chatAdapter.notifyDataSetChanged();
+                Objects.requireNonNull(chatView.getLayoutManager()).scrollToPosition(messageList.size() - 1);
+            }else {
+                Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "failed to connect!", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
